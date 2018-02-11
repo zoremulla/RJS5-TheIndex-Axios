970a6dc219b27634cf72c7e6e4895c4303d3d337
Octowl <alsaff1987@gmail.com>
2018-02-11 14:28:07 +0300

    Add an author data array

---
+++ src/data.js
@@ -0,0 +1,116 @@
+const authors = [
+  {
+    firstName: "J.K.",
+    lastName: "Rowling",
+    imageUrl: "https://nation.com.pk/digital_images/large/2015-05-10/jk-rowling-believes-in-standing-up-to-bullies-1431261222-3596.jpeg",
+    books: [
+      {
+        title: "Harry Potter and the Philosopher's Stone",
+        color: "red"
+      },
+      {
+        title: "Harry Potter and the Chamber of Secrets",
+        color: "blue"
+      },
+      {
+        title: "Harry Potter and the Prizoner of Azkaban",
+        color: "purple"
+      },
+      {
+        title: "Harry Potter and the Goblet of Fire",
+        color: "red"
+      },
+      {
+        title: "Harry Potter and the Order of the Phoenix",
+        color: "yellow"
+      },
+      {
+        title: "Harry Potter and the Half-Blood Prince",
+        color: "blue"
+      },
+      {
+        title: "Harry Potter and the Deathly Hallows",
+        color: "black"
+      }
+    ]
+  },
+  {
+    firstName: "George R.R.",
+    lastName: "Martin",
+    imageUrl: "https://typeset-beta.imgix.net/elite-daily/2016/03/06144207/elite-daily-George-RR-Martin.jpg?w=800&h=800&auto=format&q=70&fit=crop&crop=faces",
+    books: [
+      {
+        title: "A Game of Thrones",
+        color: "blue"
+      },
+      {
+        title: "A Clash of Kings",
+        color: "yellow"
+      },
+      {
+        title: "A Storm of Swords",
+        color: "green"
+      },
+      {
+        title: "A Feast for Crows",
+        color: "red"
+      },
+      {
+        title: "A Dance With Dragons",
+        color: "white"
+      },
+    ]
+  },
+  {
+    firstName: "J.R.R",
+    lastName: "Tolkein",
+    imageUrl: "https://images.8tracks.com/cover/i/010/010/920/tolkeinbigger-2362.jpg?rect=166,0,500,500&q=98&fm=jpg&fit=max",
+    books: [
+      {
+        title: "The Hobbit",
+        color: "black"
+      },
+      {
+        title: "The Fellowship of the Ring",
+        color: "green"
+      },
+      {
+        title: "The Two Towers",
+        color: "red"
+      },
+      {
+        title: "The Return of the King",
+        color: "blue"
+      },
+      {
+        title: "The Silmarillion",
+        color: "black"
+      },
+    ]
+  },
+  {
+    firstName: "Margaret",
+    lastName: "Atwood",
+    imageUrl: "https://literature.britishcouncil.org/assets/Uploads/writers/_resampled/CroppedImage680680-Margaret-Atwood-c-Jean-Malek-web.jpg",
+    books: [
+      {
+        title: "Oryx and Crake",
+        color: "green"
+      },
+      {
+        title: "The Year of the Flood",
+        color: "green"
+      },
+      {
+        title: "MaddAddam",
+        color: "blue"
+      },
+      {
+        title: "The Handmaid's Tale",
+        color: "red"
+      },
+    ]
+  }
+];
+
+export default authors;

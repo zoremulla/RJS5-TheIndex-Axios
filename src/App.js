d56d7a05e5f957a0e7ce7d1a1410aa7972fc1ab1
Octowl <alsaff1987@gmail.com>
2018-02-13 10:21:24 +0300

    Move HTML into JSX in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,68 @@
 import React, { Component } from 'react';
+import authors from './data';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div id="app" className="container-fluid">
+        <div className="row">
+          <div className="col-2">
+            <sidebar>
+              <img src="theindex.svg" className="logo" />
+              <section>
+                <h4 className="menu-item active">
+                  <a href="#">AUTHORS</a>
+                </h4>
+              </section>
+            </sidebar>
+          </div>
+          <div className="content col-10">
+            <div className="authors">
+              <h3>Authors</h3>
+              <div className="row">
+                <div className="col-4">
+                  <a href="#" className="card">
+                    <div className="image">
+                      <img className="card-img-top img-fluid" src={authors[0].imageUrl} />
+                    </div>
+                    <div className="card-body">
+                      <h5 className="card-title">
+                        <span>{authors[0].firstName + " " + authors[0].lastName}</span>
+                      </h5>
+                      <small className="card-text">{authors[0].books.length} books</small>
+                    </div>
+                  </a>
+                </div>
+                <div className="col-4">
+                  <a href="#" className="card">
+                    <div className="image">
+                      <img className="card-img-top img-fluid" src={authors[1].imageUrl} />
+                    </div>
+                    <div className="card-body">
+                      <h5 className="card-title">
+                        <span>{authors[1].firstName + " " + authors[1].lastName}</span>
+                      </h5>
+                      <small className="card-text">{authors[1].books.length} books</small>
+                    </div>
+                  </a>
+                </div>
+                <div className="col-4">
+                  <a href="#" className="card">
+                    <div className="image">
+                      <img className="card-img-top img-fluid" src={authors[2].imageUrl} />
+                    </div>
+                    <div className="card-body">
+                      <h5 className="card-title">
+                        <span>{authors[0].firstName + " " + authors[2].lastName}</span>
+                      </h5>
+                      <small className="card-text">{authors[2].books.length} books</small>
+                    </div>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }

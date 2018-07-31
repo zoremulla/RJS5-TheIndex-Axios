a7f5fb841b85008e936387bd9d1af86a3201bf6c
Octowl <alsaff1987@gmail.com>
2018-07-31 16:15:46 +0300

    Request data from API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React, { Component } from "react";
-
-// Data
-import authors from "./data";
+import axios from "axios";
 
 // Components
 import Sidebar from "./Sidebar";
+import Loading from "./Loading";
 import AuthorsList from "./AuthorsList";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      authors: [],
+      loading: true
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://the-index-api.herokuapp.com/api/authors/")
+      .then(res => res.data)
+      .then(authors => this.setState({ authors, loading: false }))
+      .catch(err => console.error(err));
+  }
+
+  getContentView() {
+    if (this.state.loading) {
+      return <Loading />;
+    } else {
+      return <AuthorsList authors={this.state.authors} />;
+    }
+  }
+
   render() {
     return (
       <div id="app" className="container-fluid">
@@ -15,9 +38,7 @@ class App extends Component {
           <div className="col-2">
             <Sidebar />
           </div>
-          <div className="content col-10">
-            <AuthorsList authors={authors} />
-          </div>
+          <div className="content col-10">{this.getContentView()}</div>
         </div>
       </div>
     );

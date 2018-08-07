f29e10076447ba2f6d3ef715bc76870153b6f323
Octowl <alsaff1987@gmail.com>
2018-08-07 16:04:24 +0300

    Move author names map outside jsx

--- src/BookRow.js
+++ src/BookRow.js
@@ -3,10 +3,11 @@ import React, { Component } from "react";
 class BookRow extends Component {
   render() {
     const book = this.props.book;
+    const authors = book.authors.map(author => author.name).join(", ");
     return (
       <tr>
         <td>{book.title}</td>
-        <td>{book.authors.map(author => author.name).join(", ")}</td>
+        <td>{authors}</td>
         <td>
           <button className="btn" style={{ backgroundColor: book.color }} />
         </td>

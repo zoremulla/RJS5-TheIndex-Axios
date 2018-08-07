0fcfa1a6f34900c4c8d3fc54ed87fe618a15283b
Octowl <alsaff1987@gmail.com>
2018-08-07 14:30:53 +0300

    Drop object assignment shorthand

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
 // Components
-import Sidebar from './Sidebar';
-import Loading from './Loading';
-import AuthorsList from './AuthorsList';
-import AuthorDetail from './AuthorDetail';
-
+import Sidebar from "./Sidebar";
+import Loading from "./Loading";
+import AuthorsList from "./AuthorsList";
+import AuthorDetail from "./AuthorDetail";
 
 const instance = axios.create({
   baseURL: "https://the-index-api.herokuapp.com"
 });
 
 class App extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       authors: [],
       filteredAuthors: [],
       loading: true,
       currentAuthor: {}
-    }
+    };
 
     this.selectAuthor = this.selectAuthor.bind(this);
     this.unselectAuthor = this.unselectAuthor.bind(this);
@@ -29,24 +27,30 @@ class App extends Component {
   }
 
   componentDidMount() {
-    instance.get('/api/authors/')
+    instance
+      .get("/api/authors/")
       .then(res => res.data)
-      .then(authors => this.setState({
-        authors,
-        filteredAuthors: authors,
-        loading: false,
-      }))
+      .then(authors =>
+        this.setState({
+          authors: authors,
+          filteredAuthors: authors,
+          loading: false
+        })
+      )
       .catch(err => console.error(err));
   }
 
   selectAuthor(authorID) {
-    this.setState({loading: true});
-    instance.get(`/api/authors/${authorID}/`)
+    this.setState({ loading: true });
+    instance
+      .get(`/api/authors/${authorID}/`)
       .then(res => res.data)
-      .then(author => this.setState({
-        currentAuthor: author,
-        loading: false
-      }))
+      .then(author =>
+        this.setState({
+          currentAuthor: author,
+          loading: false
+        })
+      )
       .catch(err => console.error(err));
   }
 
@@ -54,27 +58,33 @@ class App extends Component {
     const filteredAuthors = this.state.authors;
     this.setState({
       currentAuthor: {},
-      filteredAuthors
+      filteredAuthors: filteredAuthors
     });
   }
 
   filterAuthors(query) {
-    query = query.toLowerCase()
+    query = query.toLowerCase();
     let filteredAuthors = this.state.authors.filter(author => {
-      return `${author.first_name} ${author.last_name}`.toLowerCase().includes(query);
+      return `${author.first_name} ${author.last_name}`
+        .toLowerCase()
+        .includes(query);
     });
-    this.setState({filteredAuthors})
+    this.setState({ filteredAuthors: filteredAuthors });
   }
 
   getView() {
     if (this.state.loading) {
-      return <Loading />
+      return <Loading />;
     } else if (this.state.currentAuthor.id) {
-      return <AuthorDetail author={this.state.currentAuthor}/>
+      return <AuthorDetail author={this.state.currentAuthor} />;
     } else {
-      return <AuthorsList authors={this.state.filteredAuthors}
-                          selectAuthor={this.selectAuthor}
-                          filterAuthors={this.filterAuthors} />
+      return (
+        <AuthorsList
+          authors={this.state.filteredAuthors}
+          selectAuthor={this.selectAuthor}
+          filterAuthors={this.filterAuthors}
+        />
+      );
     }
   }
 
@@ -83,11 +93,9 @@ class App extends Component {
       <div id="app" className="container-fluid">
         <div className="row">
           <div className="col-2">
-            <Sidebar unselectAuthor={this.unselectAuthor}/>
+            <Sidebar unselectAuthor={this.unselectAuthor} />
           </div>
-          <div className="content col-10">
-            {this.getView()}
-          </div>
+          <div className="content col-10">{this.getView()}</div>
         </div>
       </div>
     );

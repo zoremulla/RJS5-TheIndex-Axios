3a5df3f06e24bf69d4a47fdf03ae62d4ba163307
Octowl <alsaff1987@gmail.com>
2018-08-11 16:13:06 +0300

    Make authors on separate lines

--- src/BookRow.js
+++ src/BookRow.js
@@ -3,7 +3,9 @@ import React, { Component } from "react";
 class BookRow extends Component {
   render() {
     const book = this.props.book;
-    const authors = book.authors.map(author => author.name).join(", ");
+    const authors = book.authors.map(author => (
+      <div key={author.name}>{author.name}</div>
+    ));
     return (
       <tr>
         <td>{book.title}</td>

0311fcb7565ca1e2060c0a1c924751f76eb2871e
Lailz <lailaabdulraheem@gmail.com>
2018-10-22 16:46:38 +0300

    Solution updated

--- src/App.js
+++ src/App.js
@@ -4,22 +4,55 @@ import authors from "./data.js";
 
 // Components
 import Sidebar from "./Sidebar";
+import Loading from "./Loading";
 import AuthorsList from "./AuthorsList";
 import AuthorDetail from "./AuthorDetail";
+
+const instance = axios.create({
+  baseURL: "https://the-index-api.herokuapp.com"
+});
 
 class App extends Component {
   state = {
     currentAuthor: null,
-    filteredAuthors: authors
+    authors: [],
+    filteredAuthors: [],
+    loading: true
   };
 
-  selectAuthor = author => this.setState({ currentAuthor: author });
+  componentDidMount() {
+    instance
+      .get("/api/authors/")
+      .then(res => res.data)
+      .then(authors =>
+        this.setState({
+          authors: authors,
+          filteredAuthors: authors,
+          loading: false
+        })
+      )
+      .catch(err => console.error(err));
+  }
+
+  selectAuthor = authorID => {
+    this.setState({ loading: true });
+    instance
+      .get(`/api/authors/${authorID}/`)
+      .then(res => res.data)
+      .then(author =>
+        this.setState({
+          currentAuthor: author,
+          loading: false
+        })
+      )
+      .catch(err => console.error(err));
+  };
 
   unselectAuthor = () => this.setState({ currentAuthor: null });
 
   filterAuthors = query => {
     query = query.toLowerCase();
-    let filteredAuthors = authors.filter(author => {
+    let filteredAuthors = this.state.authors.filter(author => {
       return `${author.first_name} ${author.last_name}`
         .toLowerCase()
         .includes(query);
@@ -28,7 +61,9 @@ class App extends Component {
   };
 
   getContentView = () => {
-    if (this.state.currentAuthor) {
+    if (this.state.loading) {
+      return <Loading />;
+    } else if (this.state.currentAuthor) {
       return <AuthorDetail author={this.state.currentAuthor} />;
     } else {
       return (
--- src/AuthorCard.js
+++ src/AuthorCard.js
@@ -5,7 +5,7 @@ function AuthorCard(props) {
   const authorName = `${author.first_name} ${author.last_name}`;
   return (
     <div className="col-lg-4 col-md-6 col-12">
-      <div className="card" onClick={() => props.selectAuthor(author)}>
+      <div className="card" onClick={() => props.selectAuthor(author.id)}>
         <div className="image">
           <img
             className="card-img-top img-fluid"

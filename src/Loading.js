525f12d471ba83ce6481e8cc6289b05719bccbc4
Octowl <alsaff1987@gmail.com>
2019-02-02 16:37:56 +0300

    missing const

--- src/Loading.js
+++ src/Loading.js
@@ -2,7 +2,7 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
-Loading = () => (
+const Loading = () => (
   <div className="spinner mx-auto text-center">
     <FontAwesomeIcon icon={faSpinner} spin size="4x" />
   </div>
